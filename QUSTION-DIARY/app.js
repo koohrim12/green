286089c8a84a438fb9d6f853b3a7d780fbf87d1a
kooharim <166689288+koohrim12@users.noreply.github.com>
2024-05-27 17:20:20 +0900

    2024-05-27 오후

--- QUSTION-DIARY/app.js
+++ QUSTION-DIARY/app.js
@@ -17,28 +17,24 @@ const server = http.createServer(function (requst, response) {
       response.write(indexJS);
       response.end();
     }
-    // logo 이미지 연결
-    if (requst.url === "/index.js") {
-      const img = fs.readFileSync("./public/logo.png");
-      response.statuCode = 200;
-      response.setHeader("content-Type", "image/javascript; charset=utf-8");
-      response.write(img);
-      response.end();
+    // else if (requst.url === "font-bg.png") {
+    //   fs.readFile("./public/font-bg.png", (err, data) => {
+    //     if (err) {
+    //       throw err;
+    //     }
+    //     response.writeHead(200, { "Content-Type": "img/png" });
+    //     response.end(data);
+    //   });
+    // }
+    else if (request.url === "/logo.png") {
+      fs.readFile("./public/logo.png", (err, data) => {
+        if (err) {
+          throw err;
+        }
+        response.writeHead(200, { "Content-Type": "img/png" });
+        response.end(data);
+      });
     }
-    // if (requst.url === "/main.html") {
-    //   const main = fs.readFileSync("./public/main.html");
-    //   response.statuCode = 200;
-    //   response.setHeader("content-Type", "text/html; charset=utf-8");
-    //   response.write(main);
-    //   response.end();
-    // }
-    // if (requst.url === "/write.html") {
-    //   const write = fs.readFileSync("./public/write.html");
-    //   response.statuCode = 200;
-    //   response.setHeader("content-Type", "text/html; charset=utf-8");
-    //   response.write(write);
-    //   response.end();
-    // }
 
     console.log(requst.url);
   }
